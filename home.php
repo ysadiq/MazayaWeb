90e3bb6b8c287693e12fa60f1cc82daae4c01920
ysaddiq <yahya.saddiq@gmail.com>
2013-03-04 17:31:06 +0200

    carousel-slider enabled

--- home.php
+++ home.php
@@ -106,23 +106,61 @@
          		<div class="accordion-group">
          			<div class="accordion-heading">
          				<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#<?php echo $key; ?>">
-         					<?php echo $value->name; ?>
+         					<img style="float:left" src="http://api.olitintl.com/APIPlatform/index.php/getImage?image=<?php echo $value->images[0]->path; ?>&width=70&height=20&gravity=center&oauth_token=66d24abb03fd436ab725d25bd76f33d2"/>
+					      	<div class="content-heading"><?php echo $value->name; ?> &nbsp </div>
+					      	<div style="clear:both"></div>
          				</a>
          			</div>
          			<div id=<?php echo $key; ?> class="accordion-body collapse" style="height: 0px;">
          				<div class="accordion-inner">
+         					<div id="myCarousel-<?php echo $key; ?>" class="carousel slide">
+         						<div class="carousel-inner">
+         							<div class="item active">
+         								<img src="http://accounts.tao.tw.shuttle.com/assets/img/bootstrap-mdo-sfmoma-01.jpg" alt="">
+         								<div class="carousel-caption">
+                            				<h4>First Thumbnail label</h4>
+                            				<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                       					 </div>
+         							</div>
+         							<div class="item">
+				                        <img src="http://accounts.tao.tw.shuttle.com/assets/img/bootstrap-mdo-sfmoma-02.jpg" alt="">
+				                        <div class="carousel-caption">
+				                            <h4>Second Thumbnail label</h4>
+				                            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+				                        </div>
+				                    </div>
+         						</div>
+         						<a class="left carousel-control" href="#myCarousel-<?php echo $key; ?>" data-slide="prev" >&lsaquo;</a>
+         						<a class="right carousel-control" href="#myCarousel-<?php echo $key; ?>" data-slide="next" >&rsaquo;</a>
+         					</div>
          					<?php var_dump($value); ?>
          				</div>
          			</div>
          		</div>
          		<?php } ?>
          	</div>	
+         	<hr>
          </div><!-- .span4 -->
          
    </div><!-- .row -->
  </div><!-- .container -->
- 	<script src="twitter-bootstrap/docs/assets/js/jquery.js"></script>
-    <script src="twitter-bootstrap/docs/assets/js/bootstrap-dropdown.js"></script>
-    <script src="twitter-bootstrap/docs/assets/js/bootstrap-collapse.js"></script>
+	
+	<script src="twitter-bootstrap/docs/assets/js/jquery.js"></script>
+	<script src="twitter-bootstrap/docs/assets/js/bootstrap-transition.js"></script>
+	<script src="twitter-bootstrap/docs/assets/js/bootstrap-alert.js"></script>
+	<script src="twitter-bootstrap/docs/assets/js/bootstrap-modal.js"></script>
+	<script src="twitter-bootstrap/docs/assets/js/bootstrap-dropdown.js"></script>
+	<script src="twitter-bootstrap/docs/assets/js/bootstrap-scrollspy.js"></script>
+	<script src="twitter-bootstrap/docs/assets/js/bootstrap-tab.js"></script>
+	<script src="twitter-bootstrap/docs/assets/js/bootstrap-tooltip.js"></script>
+	<script src="twitter-bootstrap/docs/assets/js/bootstrap-popover.js"></script>
+	<script src="twitter-bootstrap/docs/assets/js/bootstrap-button.js"></script>
+	<script src="twitter-bootstrap/docs/assets/js/bootstrap-collapse.js"></script>
+	<script src="twitter-bootstrap/docs/assets/js/bootstrap-carousel.js"></script>
+	<script src="twitter-bootstrap/docs/assets/js/bootstrap-typeahead.js"></script>
+	<script src="twitter-bootstrap/docs/assets/js/application.js"></script>
+
+
+
  </body>
 </html>
